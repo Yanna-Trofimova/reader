15dd62984faf93b2b0dfd61e221e81c980382498
Yanna-Trofimova <111576903+Yanna-Trofimova@users.noreply.github.com>
2023-03-20 01:44:47 +0200

    т

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { Reader } from 'components/Reader';
 import './index.css';
+import publications from 'publications.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Reader items={ publications} />
   </React.StrictMode>
 );
